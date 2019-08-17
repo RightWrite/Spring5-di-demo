1a35d36eda3059507d688dd552aea6845b7a3c58
RightWrite <visheshbhagat7@yahoo.com>
2019-08-17 13:33:47 +0530

    adding more services for Primary Annotation example

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -15,9 +15,9 @@ public class DiDemoApplication {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
         MyController controller = (MyController) ctx.getBean("myController");
-        controller.hello();
+//        controller.hello();
 
-
+        System.out.println(controller.hello());
         System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
         System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
         System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
