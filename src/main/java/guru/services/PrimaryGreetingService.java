a0ce7e85e97c467eb8e1997b0839f9c05d5af058
RightWrite <visheshbhagat7@yahoo.com>
2019-08-23 23:45:54 +0530

    component scan example

--- src/main/java/guru/services/PrimaryGreetingService.java
+++ src/main/java/guru/services/PrimaryGreetingService.java
@@ -1,5 +1,4 @@
-package guru.springframework.services;
-
+package guru.services;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
@@ -7,10 +6,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile("es")
-public class PrimarySpanishGreetingService implements GreetingService {
+@Profile({"en","default"})
+public class PrimaryGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hola, servicio de saludo principal ";
+        return "Hello - Primary Greeting service";
     }
 }
--- src/main/java/guru/springframework/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/controllers/SetterInjectedController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
-import guru.springframework.services.GreetingService;
+import guru.services.GreetingService;
 
 
 @Controller
